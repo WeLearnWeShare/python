99c047b6b3dbe6d79e0455f2a0934dc9aa5e644f
Colours Technologies <bala@wlws.com>
2023-12-25 13:24:33 +0530

    modified variables

--- variables/variables_int.py
+++ variables/variables_int.py
@@ -3,14 +3,14 @@ a = 10
 b = 25
 
 # Performing arithmetic operations
-sum_result = a + b
-difference = a - b
-product = a * b
+add = a + b
+sub = a - b
+multiply = a * b
 
 # Printing the results
-print(f"The sum of {a} and {b} is {sum_result}.")
+print(f"The sum of {a} and {b} is {add}.")
 
-print(f"The difference between {a} and {b} is {difference}.")
+print(f"The difference between {a} and {b} is {sub}.")
 
-print(f"The product of {a} and {b} is {product}.")
+print(f"The product of {a} and {b} is {multiply}.")
 
--- variables/variables_realtime.py
+++ variables/variables_realtime.py
@@ -1,19 +1,16 @@
-# User Registration System in Python
+username = input()
+password = input()
+email = input()
 
-# Function to register a new user
-def register_user():
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
-    email = input("Enter your email address: ")
+print(username)
+print(email)
 
-    # Here, username, password, and email are variables storing user input
 
-    # Saving the user data (For demonstration, we are just printing it)
-    print("\nRegistration Successful!")
-    print(f"Username: {username}")
-    print(f"Email: {email}")
 
-    # In a real application, this data would be stored in a database
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+email = input("Enter your email address: ")
 
-# Calling the function to register a user
-register_user()
+print("Registration Successful!")
+print("Username:" + username)
+print("Email:" + email)
